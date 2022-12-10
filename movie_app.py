cabac3d36ac28eb8cd55bd555226510d30e4f28f
YOUR_USERNAME <daniel_piciocchi@outlook.com>
2022-12-10 12:22:24 +1100

    feat: added change rating function

--- movie_app.py
+++ movie_app.py
@@ -10,13 +10,13 @@ def add_movie():
     add_movie.append(genre)
     rating = input("Please add a rating\n")
     add_movie.append(rating)
+
     with open("movie_list.csv", "a") as file:
         writer_object = writer(file)
         writer_object.writerow(add_movie)
         file.close()
 
 def delete_movie():
-   
     title = input("Which title would you like to delete?\n")
     updated_movie_list = list()
    
@@ -29,6 +29,27 @@ def delete_movie():
     with open('movie_list.csv', 'w') as writeFile:
         writer = csv.writer(writeFile)
         writer.writerows(updated_movie_list)
+
+def update_rating():
+    title = input("Which title would you like to change the rating for?\n")
+    rating = input("What is your new rating?")
+    updated_movie_list = list()
+   
+    with open("movie_list.csv", "r") as readFile:
+        rows = csv.reader(readFile)
+        for row in rows:
+            # lines.append(row)
+            if title == row[0]:
+                updated_row = [row[0], row[1] ,rating]
+                updated_movie_list.append(updated_row)
+            else:
+                updated_movie_list.append(row)
+
+                
+    with open('movie_list.csv', 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(updated_movie_list)
+
 
 
 print("Welcome to Foreign and Indie Films!")
@@ -43,6 +64,9 @@ if user_input == "1":
 
 if user_input == "2":
     delete_movie()
+
+if user_input == "3":
+    update_rating()
 
 
 

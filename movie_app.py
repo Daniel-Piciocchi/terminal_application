ccc59942091785706f341ace64ac0a228487e965
YOUR_USERNAME <daniel_piciocchi@outlook.com>
2022-12-10 11:29:44 +1100

    chore: refactored add new movie logic

--- movie_app.py
+++ movie_app.py
@@ -1,5 +1,17 @@
 from csv import writer
 
+def add_movie():
+    add_movie = []
+    title = input("Please add a film\n")
+    add_movie.append(title)
+    genre = input("Please add a genre\n")
+    add_movie.append(genre)
+    rating = input("Please add a rating\n")
+    add_movie.append(rating)
+    with open("movie_list.csv", "a") as file:
+        writer_object = writer(file)
+        writer_object.writerow(add_movie)
+        file.close()
 
 
 
@@ -10,18 +22,8 @@ print("3. Update: update rating")
 
 user_input = input()
 
-add_movie = []
+if user_input == "1":
+    add_movie()
 
-title = input("Please add a film\n")
-add_movie.append(title)
-genre = input("Please add a genre\n")
-add_movie.append(genre)
-rating = input("Please add a rating\n")
-add_movie.append(rating)
-
-with open("movie_list.csv", "a") as file:
-    writer_object = writer(file)
-    writer_object.writerow(add_movie)
-    file.close()
 
 

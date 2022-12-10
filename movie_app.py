69f8d6d2c45b8ce270b7356dd2f1e7c0f6b844de
YOUR_USERNAME <daniel_piciocchi@outlook.com>
2022-12-10 11:07:56 +1100

    feat: ability to add new movie

---
+++ movie_app.py
@@ -0,0 +1,27 @@
+from csv import writer
+
+
+
+
+print("Welcome to Foreign and Indie Films!")
+print("1. Create: add new movie")
+print("2. Delete: delete movie")
+print("3. Update: update rating")
+
+user_input = input()
+
+add_movie = []
+
+title = input("Please add a film\n")
+add_movie.append(title)
+genre = input("Please add a genre\n")
+add_movie.append(genre)
+rating = input("Please add a rating\n")
+add_movie.append(rating)
+
+with open("movie_list.csv", "a") as file:
+    writer_object = writer(file)
+    writer_object.writerow(add_movie)
+    file.close()
+
+
